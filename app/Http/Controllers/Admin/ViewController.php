3e1c0432155c9ec943fd092e245f354cd8e3009c
mc12321 <317478124@qq.com>
2018-10-09 19:16:17 +0800

    9

--- app/Http/Controllers/Admin/ViewController.php
+++ app/Http/Controllers/Admin/ViewController.php
@@ -22,7 +22,7 @@ class ViewController extends Controller
 //        $data = $request->all();
 //
        $session_val = session('wechat.oauth_user'); // 拿到授权用户资料
-dd($session_val);
+
         Log::info(__METHOD__ . " " . 'session_val:' . json_encode($session_val));
         $param = [
             'session_val' => $session_val,
--- resources/views/admin/show/index.blade.php
+++ resources/views/admin/show/index.blade.php
@@ -1,1 +1,1 @@
-<img id="" src="" style="width: 190px;height: 190px;border-radius: 50%;">
+<img id="" src="{{$session_val[0]->default->avatar}}" style="width: 190px;height: 190px;border-radius: 50%;">
